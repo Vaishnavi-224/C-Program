4c9ffd7721a4f6503f6fb58685c893c1da33d3b8
Vaishnavi-224 <vaishuraut2003@gmail.com>
2025-02-16 11:53:43 +0530

    Array

---
+++ Array/03 Array Element with Null Inialization.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+int value[7]={};
+
+printf("\n Enter a 1 Elements");
+scanf("%d",&value[0]);
+
+printf("\n Enter a 2 Elements");
+scanf("%d",&value[1]);
+
+printf("\n Enter a 3 Elements");
+scanf("%d",&value[2]);
+
+printf("\n Enter a 4 Elements");
+scanf("%d",&value[3]);
+
+printf("\n Enter a 5 Elements");
+scanf("%d",&value[4]);
+
+printf("\n Enter a 6 Elements");
+scanf("%d",&value[5]);
+
+printf("\n Enter a 7 Elements");
+scanf("%d",&value[6]);
+
+
+getch();
+
+
+
+    printf("\n value of 1 elements =%d",value[0]);
+    printf("\n value of 2 elements =%d",value[1]);
+    printf("\n value of 3 elements =%d",value[2]);
+    printf("\n value of 4 elements =%d",value[3]);
+    printf("\n value of 5 elements =%d",value[4]);
+    printf("\n value of 6 elements =%d",value[5]);
+    printf("\n value of 7 elements =%d",value[6]);
+
+
+    getch();
+return 0;
+}

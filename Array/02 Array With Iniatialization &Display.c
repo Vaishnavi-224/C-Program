31bed40865d7cfab505d649749dc9a28f961ebc4
Vaishnavi-224 <vaishuraut2003@gmail.com>
2025-02-16 11:53:06 +0530

    Add files via upload

---
+++ Array/02 Array With Iniatialization &Display.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+
+int value[5]={10,20,30,40,50};
+
+    printf("\n value of 101st elements =%d",value[0]);
+    printf("\n value of 102nd elements =%d",value[1]);
+    printf("\n value of 103rd elements =%d",value[2]);
+    printf("\n value of 104th elements =%d",value[3]);
+    printf("\n value of 105th elements =%d",value[4]);
+
+    getch();
+    return 0;
+}

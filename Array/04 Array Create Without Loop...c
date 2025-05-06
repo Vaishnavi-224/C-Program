73ebeda2ddeddf9c81f49692acaaedd74e5e9f20
Vaishnavi-224 <vaishuraut2003@gmail.com>
2025-05-06 21:40:48 +0530

    Array

---
+++ Array/04 Array Create Without Loop...c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int Num[7]={0};
+
+   printf("\n Accept value");
+
+   printf("\n Enter number 1\n");
+   scanf("%d", &Num[0]);
+
+   printf("\n Enter number 2\n");
+   scanf("%d", &Num[1]);
+
+   printf("\n Enter number 3\n");
+   scanf("%d", &Num[2]);
+
+   printf("\n Enter number 4\n");
+   scanf("%d", &Num[3]);
+
+   printf("\n Enter number 5\n");
+   scanf("%d", &Num[4]);
+
+   printf("\n Enter number 6\n");
+   scanf("%d", &Num[5]);
+
+   printf("\n Enter number 7\n");
+   scanf("%d", &Num[6]);
+
+   printf("\n Enter number 8\n");
+   scanf("%d", &Num[7]);
+
+   printf("\n Enter Execute value");
+
+   getch();
+
+   system("cls");
+
+   printf("\n value Execute is 1 = %d",Num[0]);
+
+   printf("\n value Execute is 2 = %d",Num[1]);
+
+   printf("\n value Execute is 3 = %d",Num[2]);
+
+   printf("\n value Execute is 4 = %d",Num[3]);
+
+   printf("\n value Execute is 5 = %d",Num[4]);
+
+   printf("\n value Execute is 6 = %d",Num[5]);
+
+   printf("\n value Execute is 7 = %d",Num[6]);
+
+   printf("\n value Execute is 8 = %d",Num[7]);
+
+   getch();
+   return 0;
+
+}

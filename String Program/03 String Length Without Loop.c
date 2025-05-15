909a6f44243818f4b691626a77d772afe17fa3c3
Vaishnavi Raut <vaishuraut2003@gmail.com>
2025-05-15 23:21:25 +0530

    String

---
+++ String Program/03 String Length Without Loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   char cSrc[20]= "";
+   int i = 0;
+
+   printf("\n Enter a String");
+   gets(cSrc);
+
+   if(cSrc[i] != '\0');
+   UP;
+   {
+      i++;
+      goto UP;
+   }
+
+   getch();
+   return 0;
+}

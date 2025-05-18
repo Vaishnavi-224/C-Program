752e098bd313584fc81cbc439bd7ebfc2acee145
Vaishnavi Raut <vaishuraut2003@gmail.com>
2025-05-18 22:53:27 +0530

    Basic

---
+++ Basic Program/14 Celcius.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Fht = 0.0, Cel = 0.0;
+
+    printf("\n Enter  Temperature in  Fahrenheit : ");
+    scanf("%f",&Fht);
+
+    Cel = ( Fht - 32)  * (5.0/9.0);
+
+    printf("\n\n Temperature %0.2f celcius = %0.2f Celsius.", Fht,Cel);
+
+    getch();
+    return 0;
+}
+

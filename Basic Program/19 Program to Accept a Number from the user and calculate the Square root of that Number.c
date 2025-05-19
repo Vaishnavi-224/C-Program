dbed65fe5da80c9c2a590a12a642749f2a75f184
Vaishnavi Raut <vaishuraut2003@gmail.com>
2025-05-19 14:11:50 +0530

    Basic

---
+++ Basic Program/19 Program to Accept a Number from the user and calculate the Square root of that Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int No=0,SquareRoot;
+
+  printf("\n Enter a Number");
+  scanf("%d",&No);
+
+  SquareRoot = Sqrt(No);
+
+
+  printf("\n SquareRoot of  number is  %d = %d "No,SquareRoot);
+  getch();
+  return 0;
+}

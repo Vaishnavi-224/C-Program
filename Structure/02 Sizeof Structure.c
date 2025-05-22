929512482902cad66738f7cb25e5351ea1be87cf
Vaishnavi Raut <vaishuraut2003@gmail.com>
2025-05-22 20:47:33 +0530

    Structure

---
+++ Structure/02 Sizeof Structure.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct stud
+{
+     char Name[8];
+     char City[10];
+     int RNO;
+     float Per;
+};
+
+int main()
+{
+    struct stud std;
+
+    printf("\n Size of Structure Object : %d.", sizeof(std));
+
+    getch();
+    return 0;
+
+}

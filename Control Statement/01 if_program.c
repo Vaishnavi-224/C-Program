4a28cd7d9b1c9e67fc3dde7d92764f69a722e998
Vaishnavi Raut <vaishuraut2003@gmail.com>
2025-05-23 16:12:51 +0530

    CS

---
+++ Control Statement/01 if_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no=0;
+    printf("\n enter a positive number : ");
+    scanf("%d",&no);
+    if(no>0)
+    {
+        printf("\n positive no");
+    }
+    printf("\n Thank you");
+    getch();
+    return 0;
+}
